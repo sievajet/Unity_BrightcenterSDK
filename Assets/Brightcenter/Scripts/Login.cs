32bb877232f01dfa5a2877df119898078e27118f
sievajet <alltehmagic@gmail.com>
2015-10-29 15:00:25 +0100

    Made a start with a login sdk

---
+++ Assets/Brightcenter/Scripts/Login.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Login : MonoBehaviour {
+
+    private string u = "username";
+    private string p = "password";
+
+    private string usernameString = string.Empty;
+    private string passwordString = string.Empty;
+
+    void Start () {
+	
+	}
+
+	void Update () {
+   
+	}
+
+    void OnGui()
+    {
+        
+    }
+
+    
+    }
